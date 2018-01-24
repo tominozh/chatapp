ba8cd0b846e9553090f291d617677d1ae9b485dc
Tomas Haladek <18346449+tominozh@users.noreply.github.com>
2018-01-24 22:20:07 +0000

    1. client updated

--- Client.java
+++ Client.java
@@ -1,90 +1,60 @@
-import java.io.DataInputStream;
-import java.io.PrintStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+package ChatApp;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+/*
+*@Author Tomas Haladek
+*/
 
 public class Client implements Runnable {
+	final static int ServerPort = 15566;
+	static int counter = 0;
+	static DataInputStream dis = null;
+	static DataOutputStream dos = null;
+	static boolean isAlive = false;
 
- // The client socket
-  private static Socket clientSocket = null;
-  // The output stream
-  private static PrintStream os = null;
-  // The input stream
-  private static DataInputStream is = null;
+	public static void main(String args[]) throws UnknownHostException, IOException {
+		Scanner scn = new Scanner(System.in);
+		// establish the connection
+		Socket socket = new Socket("localhost", ServerPort);
+		if (socket.isConnected()) {
+			// obtaining input and out streams
+			dis = new DataInputStream(socket.getInputStream());
+			dos = new DataOutputStream(socket.getOutputStream());
+			isAlive = true;
+		}
+		//if all good
+		if (socket.isConnected() && dis != null && dos != null) {
+           //new thread to read and write message
+			new Thread(new Client()).start();
+			while (isAlive) {
+				dos.writeUTF(scn.nextLine());
+			}
+			dos.close();
+			dis.close();
+			socket.close();
+		}
+	}
 
-  private static BufferedReader inputLine = null;
-  private static boolean closed = false;
-  
-  public static void main(String[] args) {
 
-    // The default port.
-    int portNumber = 2222;
-    // The default host.
-    String hostIP = "localhost";
-    
-     // Open a socket on a given host and port. Open input and output streams.
-    try {
-      clientSocket = new Socket(hostIP, portNumber);
-      inputLine = new BufferedReader(new InputStreamReader(System.in));
-      os = new PrintStream(clientSocket.getOutputStream());
-      is = new DataInputStream(clientSocket.getInputStream());
-      
-  	} catch (UnknownHostException e) {
-      System.err.println("Don't know about host " + hostIP);
-    } catch (IOException e) {
-      System.err.println("Couldn't get I/O for the connection to the host "+ hostIP);
-    }
+	@Override
+	public void run() {
+		String responseLine;
+		try {
+			while (isAlive) {
+				responseLine = dis.readUTF();
+				if (!responseLine.isEmpty()) {
+					
+					System.out.println(responseLine);
+				}
 
-    /*
-     * If everything has been initialized then we want to write some data to the
-     * socket we have opened a connection to on the port portNumber.
-     */
-    if (clientSocket != null && os != null && is != null) {
-      try {
+				if (responseLine.contains("Bye"))
+					isAlive = false;
+			}
+		} catch (Exception e) {
 
-        /* Create a thread to read from the server. */
-        new Thread(new Client()).start();
-        while (!closed) {        	
-        
-          os.println(inputLine.readLine().trim());
-        }
-        /*
-         * Close the output stream, close the input stream, close the socket.
-         */
-        os.close();
-        is.close();
-        clientSocket.close();
-      } catch (IOException e) {
-        System.err.println("IOException:  " + e);
-      }
-    }
-  }
+		}
 
-  /*
-   * Create a thread to read from the server. (non-Javadoc)
-   * 
-   * @see java.lang.Runnable#run()
-   */
-  public void run() {
-    /*
-     * Keep on reading from the socket till we receive "Bye" from the
-     * server. Once we received that then we want to break.
-     */
-    String responseLine;
-    try {
-      while ((responseLine = is.readLine()) != null) {
-        System.out.println(responseLine);
-        if (responseLine.indexOf("*** Bye") != -1)
-          break;
-      }
-      closed = true;
-    } catch (IOException e) {
-      System.err.println("IOException:  " + e);
-    }
-  }
+	}
 }
